ed7b96f389640d34c6daf0c27af42857159956d3
webvelocityZA <webvelocityapps@gmail.com>
2022-06-04 01:37:12 +0200

    Initial commit

---
+++ src/app/components/calculator/calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.scss']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
